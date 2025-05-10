2eaecc37de43d3ba7a8953dad854c2ab42986078
POAPCA02LC0814 <SSDD@POAPCA02LC0814.duoc.acad>
2025-05-10 13:57:36 -0400

    Codigo final clase 10.05.2025

--- src/main/java/com/rrhh/gestion/controller/EmpleadoController.java
+++ src/main/java/com/rrhh/gestion/controller/EmpleadoController.java
@@ -1,5 +1,6 @@
 package com.rrhh.gestion.controller;
 
+import com.rrhh.gestion.dto.EmpleadoDTO;
 import com.rrhh.gestion.entity.Empleado;
 import com.rrhh.gestion.repository.EmpleadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping ("/api/empleados/")
@@ -22,4 +24,19 @@ public class EmpleadoController {
     public List<Empleado> leerEmpleado() {
         return empleadoRepository.findAll();
     }
+
+    @GetMapping("/comuna/")
+    public List<EmpleadoDTO> leerEmpleadoDTO(){
+        List<Empleado> empleados = empleadoRepository.findAll();
+        List<EmpleadoDTO> empleadoDTO = new ArrayList<>();
+        for (Empleado emp : empleados){
+            empleadoDTO.add(new EmpleadoDTO(emp.getRut(),
+                                            emp.getNombre(),
+                                            emp.getTelefono(),
+                                            emp.getCorreo(),
+                                            emp.getComuna().getNombre()));
+        }
+        return  empleadoDTO;
+    }
+
 }
